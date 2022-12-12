37ead67ea1efbfa4b4f07fd9b104af0880464502
FontJordi <jordifontmiralles@gmail.com>
2022-12-12 18:13:55 +0100

    write stream data to csv, needs cleanup and understanding

--- twitter/classes/import_twitter.py
+++ twitter/classes/import_twitter.py
@@ -39,7 +39,7 @@ class MyStream(tweepy.StreamingClient):
             #producer.send("TutorialTopic", value="hello")
             print(tweet.text)
 
-            time.sleep(1)
+            time.sleep(10)
 
     def start_streaming_tweets(self, search_term):
 
@@ -54,49 +54,3 @@ if __name__ == "__main__":
     twitter_stream = MyStream(bearer_token=bearer_token)
     twitter_stream.start_streaming_tweets("twitter")
     #twitter_stream.filter()
-
-#stream = MyStream(bearer_token=bearer_token)
-#stream.filter(tweet_fields = ["referenced_tweets"])
-
-#print(type(stream.filter(tweet_fields = ["referenced_tweets"])))
-#stream.sample()
-
-#stream.add_rules(tweepy.StreamRule("lol"))
-
-#stream.filter(tweet_fields = ["referenced_tweets"])
-
-
-#Response(data=[StreamRule(value='Sonny Boy', tag=None, id='1601935263453925376'), StreamRule(value='Argentina', tag=None, id='1601940833397260290'), 
-#StreamRule(value='Messi', tag=None, id='1601941898003898368')], includes={}, errors=[], meta={'sent': '2022-12-11T14:10:25.224Z', 'result_count': 3})
-
-
-
-"""
-#query = 'from:kiwichi__ -is:retweet'
-#query = 'from:kiwichi__ '
-
-#kiwichi = client.get_user(username="kiwichi__")
-#tweets = client.search_recent_tweets(query=query, tweet_fields=['context_annotations', 'created_at'], max_results=100)
-
-##print(kiwichi.data.id)
-#likes = client.get_liked_tweets(kiwichi.data.id)
- 
-#dict1 = likes._asdict()
-"""
-
-
-
-
-
-#fm.writeResponse("/home/kiwichi/Documents/Projecte/data", "likeskiwichi", dict1)
-
-#for like in likes.data:
-#    print(like.text + "\n")
-
-#for tweet in tweets.data:
-#    print(tweet.text + "\n")
-    #if len(tweet.context_annotations) > 0:
-    #   print(tweet.context_annotations)
-
-
-
--- twitter/jobs/main.py
+++ twitter/jobs/main.py
@@ -4,22 +4,46 @@ import pyspark
 import pyspark.sql
 from typing import Callable
 
-conf = "spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1"
+conf = "spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0"
+
+scala_version = '2.12'
+spark_version = '3.3.0'
+
+packages = [
+    f'org.apache.spark:spark-sql-kafka-0-10_{scala_version}:{spark_version}',
+    'org.apache.kafka:kafka-clients:3.3.0'
+]
 
 if __name__ == "__main__":
     
     spark = pyspark.sql.SparkSession \
         .builder \
-        .appName("Spark stream twitter data") \
-        .config(conf = conf) \
+        .appName("SparkTwitter") \
+        .config("spark.jars.packages", ",".join(packages))\
         .getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
 
+    print("hi")
 
     df = spark \
-        .readStream \
-        .format("kafka") \
-        .option("kafka.bootstrap.servers", "localhost:9092") \
-        .load()
+    .readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("subscribe", "TutorialTopic") \
+    .load()
+
+    df2 = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
     
-    
+    df2.writeStream \
+        .format("csv") \
+        .trigger(processingTime="60 seconds") \
+        .outputMode("append") \
+        .option("checkpointLocation", "checkpoint") \
+        .option("path", "output_path/") \
+        .start() \
+        .awaitTermination(timeout=70)
+
+##s'ha d'afegir una opcio de que si ja existeix un single csv file, appendd
+
+allfiles =  spark.read.option("header","false").csv("/home/kiwichi/Documents/Projecte/output_path/part-*.csv")    
+allfiles.coalesce(1).write.format("csv").option("header", "false").save("/home/kiwichi/Documents/Projecte/data/single_csv_file3/")
